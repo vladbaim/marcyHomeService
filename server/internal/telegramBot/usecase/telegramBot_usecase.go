8b6cb91ec5170f79d1f67f7e30e2a156a5b838f4
Баймурзин Владислав Олегович <vobaimurzin@domclick.ru>
2024-04-06 13:45:18 +0400

    Формат времени

--- server/internal/telegramBot/usecase/telegramBot_usecase.go
+++ server/internal/telegramBot/usecase/telegramBot_usecase.go
@@ -26,14 +26,14 @@ func (t *telegramBotUsecase) SendChatMessage(ctx context.Context, chatId int64, 
 		return
 	}
 
-	formattedTime := fmt.Sprintf("%d-%02d-%02dT%02d:%02d:%02d",
-		sensorData.CreatedAt.Year(), sensorData.CreatedAt.Month(), sensorData.CreatedAt.Day(),
+	formattedTime := fmt.Sprintf("%02d.%02d %02d:%02d:%02d",
+	sensorData.CreatedAt.Day(), sensorData.CreatedAt.Month(),
 		sensorData.CreatedAt.Hour(), sensorData.CreatedAt.Minute(), sensorData.CreatedAt.Second())
 	sendedMessage, err = t.telegramBot.Send(
 		tgbotapi.NewMessage(
 			chatId,
 			message+
-				fmt.Sprintf("\nТемпература: %.1fC \n Влажность: %.1f%% \n Уровень CO2: %d", sensorData.Temperature, sensorData.Humidity, sensorData.CarbonDioxide)+
+				fmt.Sprintf("\nТемпература: %.1fC \nВлажность: %.1f%% \nУровень CO2: %d", sensorData.Temperature, sensorData.Humidity, sensorData.CarbonDioxide)+
 				"\nВремя показателей: "+
 				formattedTime))
 	return
